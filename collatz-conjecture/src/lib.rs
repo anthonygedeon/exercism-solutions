7d94118911cb6dc2c2756fad4114b133ddc099f5
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-02-02 23:22:44 -0600

    refactor collatz-conjecture exercise
    saw someone else solution and was amazing with what you could do with match

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,28 +1,12 @@
 pub fn collatz(mut n: u64) -> Option<u64> {
-    let mut i = 0;
-    while n != 1 {
-        if n % 2 == 0 {
-            n = n / 2;
-        } else {
-            let (n1, of1) = n.overflowing_mul(3);
-            let (n2, of2) = n1.overflowing_add(1);
-            if of1 || of2 {return None} 
-            n = n2;
+    for steps in 0.. {
+        match n {
+            0 => break, 
+            1 => return Some(steps), 
+            even if even % 2 == 0 => n = n / 2, 
+            _ => n = n.checked_mul(3)?.checked_add(1)?, 
         }
-
-        if n >= u64::MAX || n == 0 {
-            return None;
-        }
-
-        match n.checked_add(n) {
-            Some(_) => (), 
-            None => return None, 
-        }
-        match n.checked_mul(n) {
-            Some(_) => (), 
-            None => return None, 
-        }
-        i += 1;
     }
-    Some(i)
+
+    None
 }
