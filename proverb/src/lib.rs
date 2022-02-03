5a1a8ab3f9d87ee141c4debcf79c4d772d5a74bd
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-02-02 23:23:04 -0600

    refactor proverb exercise

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,22 +1,12 @@
+use std::iter::once;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    let mut proverb: Vec<String> = Vec::new();
-    let mut last_word = String::new();
-    for (i, word) in list.iter().enumerate() {
-        if list.len() > 1 {
-            let word2 = match list.get(i + 1) {
-                None => break,
-                Some(word) => word.to_string(),
-            };
-            let sentence = format!("For want of a {} the {} was lost.", word, word2);
-            proverb.push(sentence);
-        }
-        last_word = list.first().unwrap().to_string();
+    match list.first() {
+        Some(word) => list
+            .windows(2)
+            .map(|chunk| format!("For want of a {} the {} was lost.\n", chunk[0], chunk[1]))
+            .chain(once(format!("And all for the want of a {}.", word)))
+            .collect(),
+        None => String::new(),
     }
-    if !list.is_empty() {
-        proverb.push(format!("And all for the want of a {}.", &last_word));
-    }
-    if proverb.is_empty() {
-        return String::new();
-    }
-    return proverb.join("\n");
 }
