57eed7d9e24f55c20098fbe8fa322e43893dd5cc
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-03-09 23:58:51 -0600

    wip solution

---
+++ armstrong-numbers/armstrong_numbers.c
@@ -0,0 +1,22 @@
+#include "armstrong_numbers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+/**
+ *
+ */
+bool is_armstrong_number(int candidate) {
+       
+    int sum = 0;
+    const char *str = candidate;
+    printf("%ld", strlen(str));
+    for (int i = 0; i < ; i++) {
+        
+    }
+        
+    return false;
+       
+}
